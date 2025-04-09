0fa65a8079b08963c6671e5115045e5e33d89690
Hunter <hunterhartline@gmail.com>
2025-04-08 21:35:29 -0500

    fix dist paths for assets;

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -7,10 +7,10 @@ import { execSync } from 'child_process';
 
 try {
   console.log('Copying templates...');
-  execSync('cp -R src/templates dist/templates', { stdio: 'inherit' });
+  execSync('cp -R src/assets/templates dist/assets/templates', { stdio: 'inherit' });
 
   console.log('Copying examples...');
-  execSync('cp -R src/examples dist/examples', { stdio: 'inherit' });
+  execSync('cp -R src/assets/PulumiExamples dist/assets/PulumiExamples', { stdio: 'inherit' });
 
   console.log('Assets copied successfully.');
 } catch (error) {
